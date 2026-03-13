063b5a07317b36df239bac6b4562eca338e0d459
Aurimas <aurimas.golotylecas@workofo.com>
2026-03-13 17:30:45 +0200

    Add application-test.properties and update MealPlannerApplicationTests

--- src/test/java/com/recipe/meal_planner/MealPlannerApplicationTests.java
+++ src/test/java/com/recipe/meal_planner/MealPlannerApplicationTests.java
@@ -2,8 +2,10 @@ package com.recipe.meal_planner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MealPlannerApplicationTests {
 
 	@Test
